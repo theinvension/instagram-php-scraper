966da98cccabf4d62e94c45abbe4ea684af91a5e
Anton Sidashin <russiancss@gmail.com>
2020-09-15 22:24:00 +0400

    Update Endpoints.php

--- src/InstagramScraper/Endpoints.php
+++ src/InstagramScraper/Endpoints.php
@@ -4,43 +4,44 @@ namespace InstagramScraper;
 
 class Endpoints
 {
-    const BASE_URL = 'https://www.instagram.com';
-    const LOGIN_URL = 'https://www.instagram.com/accounts/login/ajax/';
-    const ACCOUNT_PAGE = 'https://www.instagram.com/{username}';
-    const MEDIA_LINK = 'https://www.instagram.com/p/{code}';
-    const ACCOUNT_MEDIAS = 'https://www.instagram.com/graphql/query/?query_hash=e769aa130647d2354c40ea6a439bfc08&variables={variables}';
-    const ACCOUNT_JSON_INFO = 'https://www.instagram.com/{username}/?__a=1';
-    const ACCOUNT_ACTIVITY = 'https://www.instagram.com/accounts/activity/?__a=1';
-    const MEDIA_JSON_INFO = 'https://www.instagram.com/p/{code}/?__a=1';
-    const MEDIA_JSON_BY_LOCATION_ID = 'https://www.instagram.com/explore/locations/{{facebookLocationId}}/?__a=1&max_id={{maxId}}';
-    const MEDIA_JSON_BY_TAG = 'https://www.instagram.com/explore/tags/{tag}/?__a=1&max_id={max_id}';
-    const GENERAL_SEARCH = 'https://www.instagram.com/web/search/topsearch/?query={query}&count={count}';
+    const PROXY_URL = 'http://127.0.0.1:3339';
+    const BASE_URL = PROXY_URL . '';
+    const LOGIN_URL = PROXY_URL . '/accounts/login/ajax/';
+    const ACCOUNT_PAGE = PROXY_URL . '/{username}';
+    const MEDIA_LINK = PROXY_URL . '/p/{code}';
+    const ACCOUNT_MEDIAS = PROXY_URL . '/graphql/query/?query_hash=e769aa130647d2354c40ea6a439bfc08&variables={variables}';
+    const ACCOUNT_JSON_INFO = PROXY_URL . '/{username}/?__a=1';
+    const ACCOUNT_ACTIVITY = PROXY_URL . '/accounts/activity/?__a=1';
+    const MEDIA_JSON_INFO = PROXY_URL . '/p/{code}/?__a=1';
+    const MEDIA_JSON_BY_LOCATION_ID = PROXY_URL . '/explore/locations/{{facebookLocationId}}/?__a=1&max_id={{maxId}}';
+    const MEDIA_JSON_BY_TAG = PROXY_URL . '/explore/tags/{tag}/?__a=1&max_id={max_id}';
+    const GENERAL_SEARCH = PROXY_URL . '/web/search/topsearch/?query={query}&count={count}';
     const ACCOUNT_JSON_INFO_BY_ID = 'ig_user({userId}){id,username,external_url,full_name,profile_pic_url,biography,followed_by{count},follows{count},media{count},is_private,is_verified}';
-    const COMMENTS_BEFORE_COMMENT_ID_BY_CODE = 'https://www.instagram.com/graphql/query/?query_hash=33ba35852cb50da46f5b5e889df7d159&variables={variables}';
+    const COMMENTS_BEFORE_COMMENT_ID_BY_CODE = PROXY_URL . '/graphql/query/?query_hash=33ba35852cb50da46f5b5e889df7d159&variables={variables}';
     const LAST_LIKES_BY_CODE = 'ig_shortcode({{code}}){likes{nodes{id,user{id,profile_pic_url,username,follows{count},followed_by{count},biography,full_name,media{count},is_private,external_url,is_verified}},page_info}}';
-    const LIKES_BY_SHORTCODE = 'https://www.instagram.com/graphql/query/?query_id=17864450716183058&variables={"shortcode":"{{shortcode}}","first":{{count}},"after":"{{likeId}}"}';
-    const FOLLOWING_URL = 'https://www.instagram.com/graphql/query/?query_id=17874545323001329&id={{accountId}}&first={{count}}&after={{after}}';
-    const FOLLOWERS_URL = 'https://www.instagram.com/graphql/query/?query_id=17851374694183129&id={{accountId}}&first={{count}}&after={{after}}';
-    const FOLLOW_URL = 'https://www.instagram.com/web/friendships/{{accountId}}/follow/';
-    const UNFOLLOW_URL = 'https://www.instagram.com/web/friendships/{{accountId}}/unfollow/';
-    const USER_FEED = 'https://www.instagram.com/graphql/query/?query_id=17861995474116400&fetch_media_item_count=12&fetch_media_item_cursor=&fetch_comment_count=4&fetch_like=10';
-    const USER_FEED2 = 'https://www.instagram.com/?__a=1';
-    const INSTAGRAM_QUERY_URL = 'https://www.instagram.com/query/';
+    const LIKES_BY_SHORTCODE = PROXY_URL . '/graphql/query/?query_id=17864450716183058&variables={"shortcode":"{{shortcode}}","first":{{count}},"after":"{{likeId}}"}';
+    const FOLLOWING_URL = PROXY_URL . '/graphql/query/?query_id=17874545323001329&id={{accountId}}&first={{count}}&after={{after}}';
+    const FOLLOWERS_URL = PROXY_URL . '/graphql/query/?query_id=17851374694183129&id={{accountId}}&first={{count}}&after={{after}}';
+    const FOLLOW_URL = PROXY_URL . '/web/friendships/{{accountId}}/follow/';
+    const UNFOLLOW_URL = PROXY_URL . '/web/friendships/{{accountId}}/unfollow/';
+    const USER_FEED = PROXY_URL . '/graphql/query/?query_id=17861995474116400&fetch_media_item_count=12&fetch_media_item_cursor=&fetch_comment_count=4&fetch_like=10';
+    const USER_FEED2 = PROXY_URL . '/?__a=1';
+    const INSTAGRAM_QUERY_URL = PROXY_URL . '/query/';
     const INSTAGRAM_CDN_URL = 'https://scontent.cdninstagram.com/';
     const ACCOUNT_JSON_PRIVATE_INFO_BY_ID = 'https://i.instagram.com/api/v1/users/{userId}/info/';
-    const ACCOUNT_JSON_PRIVATE_INFO_BY_ID_2 = 'https://www.instagram.com/graphql/query/?query_hash=c9100bf9110dd6361671f113dd02e7d6&variables={"user_id":"{userId}","include_chaining":false,"include_reel":true,"include_suggested_users":false,"include_logged_out_extras":false,"include_highlight_reels":false,"include_related_profiles":false}';
-    const LIKE_URL = 'https://www.instagram.com/web/likes/{mediaId}/like/';
-    const UNLIKE_URL = 'https://www.instagram.com/web/likes/{mediaId}/unlike/';
-    const ADD_COMMENT_URL = 'https://www.instagram.com/web/comments/{mediaId}/add/';
-    const DELETE_COMMENT_URL = 'https://www.instagram.com/web/comments/{mediaId}/delete/{commentId}/';
-    const ACCOUNT_MEDIAS2 = 'https://www.instagram.com/graphql/query/?query_id=17880160963012870&id={{accountId}}&first=10&after=';
-    const HIGHLIGHT_URL = 'https://www.instagram.com/graphql/query/?query_hash=c9100bf9110dd6361671f113dd02e7d6&variables={"user_id":"{userId}","include_chaining":false,"include_reel":true,"include_suggested_users":false,"include_logged_out_extras":false,"include_highlight_reels":true,"include_live_status":false}';
-    const THREADS_URL = 'https://www.instagram.com/direct_v2/web/inbox/?persistentBadging=true&folder=&limit={limit}&thread_message_limit={messageLimit}&cursor={cursor}';
+    const ACCOUNT_JSON_PRIVATE_INFO_BY_ID_2 = PROXY_URL . '/graphql/query/?query_hash=c9100bf9110dd6361671f113dd02e7d6&variables={"user_id":"{userId}","include_chaining":false,"include_reel":true,"include_suggested_users":false,"include_logged_out_extras":false,"include_highlight_reels":false,"include_related_profiles":false}';
+    const LIKE_URL = PROXY_URL . '/web/likes/{mediaId}/like/';
+    const UNLIKE_URL = PROXY_URL . '/web/likes/{mediaId}/unlike/';
+    const ADD_COMMENT_URL = PROXY_URL . '/web/comments/{mediaId}/add/';
+    const DELETE_COMMENT_URL = PROXY_URL . '/web/comments/{mediaId}/delete/{commentId}/';
+    const ACCOUNT_MEDIAS2 = PROXY_URL . '/graphql/query/?query_id=17880160963012870&id={{accountId}}&first=10&after=';
+    const HIGHLIGHT_URL = PROXY_URL . '/graphql/query/?query_hash=c9100bf9110dd6361671f113dd02e7d6&variables={"user_id":"{userId}","include_chaining":false,"include_reel":true,"include_suggested_users":false,"include_logged_out_extras":false,"include_highlight_reels":true,"include_live_status":false}';
+    const THREADS_URL = PROXY_URL . '/direct_v2/web/inbox/?persistentBadging=true&folder=&limit={limit}&thread_message_limit={messageLimit}&cursor={cursor}';
 
     // Look alike??
-    const URL_SIMILAR = 'https://www.instagram.com/graphql/query/?query_id=17845312237175864&id=4663052';
+    const URL_SIMILAR = PROXY_URL . '/graphql/query/?query_id=17845312237175864&id=4663052';
 
-    const GRAPH_QL_QUERY_URL = 'https://www.instagram.com/graphql/query/?query_id={{queryId}}';
+    const GRAPH_QL_QUERY_URL = PROXY_URL . '/graphql/query/?query_id={{queryId}}';
 
     private static $requestMediaCount = 30;
 
